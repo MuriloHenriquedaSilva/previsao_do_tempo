9f0db3b0a21e7117b324af3e8126e428491361fb
MuriloHenriquedaSilva <84098523+MuriloHenriquedaSilva@users.noreply.github.com>
2021-06-14 11:22:46 -0300

    Commit final

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+//import 'package:previsao_do_tempo/provider/weatcher_server.dart';
+//import 'package:previsao_do_tempo/screens/home/widgets/main_screen.dart';
+import 'package:previsaodotempo/provider/weatcher_server.dart';
+import 'package:previsaodotempo/screens/home/widgets/main_screen.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getData(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(
+                    Colors.white,
+                  ),
+                ),
+              ),
+            );
+          default:
+            return MainScreen(snapshot.data["results"]);
+        }
+      },
+    );
+  }
+}
